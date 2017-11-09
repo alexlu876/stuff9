166dfecf82a248ece1c6d9ad744888a95c302bf5
Alex Lu <Alex@Alexs-MacBook-Pro-2.local>
2017-11-09 09:46:10 -0500

    lit

--- bar.c
+++ bar.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 static void sighandler(int signo){
   if(signo == SIGINT){  
     printf("Oh no! SIGINT is attacking! Run away!!\n");
+    int fd;
+    fd = open("foo.txt", O_CREAT | O_WRONLY | O_TRUNC, 0777);
+    write(fd, "SIGINT has ended the process!", 256);
+    close(fd);
     exit(1);
   }
   if(signo == SIGUSR1){
